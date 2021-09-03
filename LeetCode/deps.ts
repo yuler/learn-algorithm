6ad899bcc78116462faf9531a446699a84d6cc0f
yuler <joyful4yu@gmail.com>
2021-09-03 18:06:14 +0800

    wip: fetch script

---
+++ LeetCode/deps.ts
@@ -0,0 +1,3 @@
+import * as asserts from 'https://deno.land/std@0.106.0/testing/asserts.ts'
+
+export {asserts}
---
+++ LeetCode/first-bad-version/first-bad-version.ts
@@ -0,0 +1,18 @@
+/**
+ * The knows API is defined in the parent class Relation.
+ * isBadVersion(version: number): boolean {
+ *     ...
+ * };
+ */
+
+var solution = function (isBadVersion: any) {
+	return function (n: number): number {
+		return 0
+	}
+}
+
+// test cases
+import {asserts} from '../deps.ts'
+Deno.test('test', () => {
+	asserts.equal(0, 0)
+})
