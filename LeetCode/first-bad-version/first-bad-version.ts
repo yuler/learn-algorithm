a38e2757b53b7ee91c88af6eecdb6aed87f3a198
yuler <joyful4yu@gmail.com>
2021-09-06 13:31:49 +0800

    Add `278: first-bad-version` typescript solution

--- LeetCode/first-bad-version/first-bad-version.ts
+++ LeetCode/first-bad-version/first-bad-version.ts
@@ -5,14 +5,30 @@
  * };
  */
 
-var solution = function (isBadVersion: any) {
+var solution = function (isBadVersion: (n: number) => boolean) {
 	return function (n: number): number {
-		return 0
+		let left = 1
+		let right = n
+		while (left < right) {
+			const mid = Math.floor(left + (right - left) / 2)
+			if (isBadVersion(mid)) {
+				right = mid
+			} else {
+				left = mid + 1
+			}
+		}
+		return left
 	}
 }
 
 // test cases
-import {asserts} from '../deps.ts'
-Deno.test('test', () => {
-	asserts.equal(0, 0)
+import {asserts} from '../../deps.ts'
+Deno.test('n = 5, bad = 4, output = 4', () => {
+	const isBadVersion = (n: number) => n >= 4
+	asserts.assertEquals(solution(isBadVersion)(5), 4)
+})
+
+Deno.test('n = 1, bad = 1, output = 4', () => {
+	const isBadVersion = (n: number) => n >= 1
+	asserts.assertEquals(solution(isBadVersion)(1), 1)
 })
